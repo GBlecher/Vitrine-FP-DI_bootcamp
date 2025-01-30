15d3229493175e8af9758d949c516fb61ce72a4c
GBlecher <gilanblecher@gmail.com>
2025-01-30 03:09:44 +0200

    test deploy back

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 
 
 const userRouter = require("./routes/userRouter.js")
 const postRouter = require("./routes/postRouter.js")
 
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
 const { PORT } = process.env;
 app.listen(PORT || 5001, () => {
   console.log(`run on ${PORT || 5001}`);
@@ -22,7 +27,9 @@ app.use(
   })
 );
 
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
