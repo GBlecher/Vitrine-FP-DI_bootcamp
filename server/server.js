2250fe2736943229993055ce72b8562f5c614467
GBlecher <gilanblecher@gmail.com>
2025-02-01 20:13:43 +0200

    zdr

--- server/server.js
+++ server/server.js
@@ -8,8 +8,19 @@ const app = express();
 const userRouter = require("./routes/userRouter.js")
 const postRouter = require("./routes/postRouter.js")
 
+app.use(cors({
+  credentials: true,
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173'
+}));
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, './client/dist')));
+
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/dist/index.html'));
@@ -21,20 +32,5 @@ app.listen(PORT || 5001, () => {
   console.log(`run on ${PORT || 5001}`);
 });
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:5173']
-    // origin: ['https://vitrine-df45.onrender.com']
-  })
-);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.use("/api/user", userRouter);
-app.use("/api/post", postRouter);
 
